7903e691cbd15653955877aeb72a9cad64569149
FadyShehataa <fadyshehata40@gmail.com>
2023-11-06 21:38:55 +0200

    Edit initial profile & cover images

--- lib/Features/Profile/presentation/views/profile_view.dart
+++ lib/Features/Profile/presentation/views/profile_view.dart
@@ -1,14 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:social_app/Features/Profile/presentation/views/widgets/profile_actions.dart';
 import 'package:social_app/Features/Profile/presentation/views/widgets/profile_bio.dart';
 import 'package:social_app/Features/Profile/presentation/views/widgets/profile_header.dart';
 import 'package:social_app/Features/Profile/presentation/views/widgets/profile_stats.dart';
 
+import '../../../../Core/utils/constants.dart';
+
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    print(user);
     return const Column(
       children: [
         ProfileHeader(),
